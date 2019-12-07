fe21229ea6c34debe051da3c4368a87c7ec774f1
Mehmet Yurtar <mehmetyurtar@gmail.com>
2019-12-07 14:06:54 +0300

    Updates

---
+++ precache-manifest.a2b70547966ce2d601d162449d4264fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eca5376323f8114b300b97febe8856d7",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "b36c8341ddc7451f632c",
+    "url": "/todoapp/static/css/main.1b7c0b81.chunk.css"
+  },
+  {
+    "revision": "b49dc619e7af90924616",
+    "url": "/todoapp/static/js/2.17d1f453.chunk.js"
+  },
+  {
+    "revision": "b36c8341ddc7451f632c",
+    "url": "/todoapp/static/js/main.e84cde52.chunk.js"
+  },
+  {
+    "revision": "d1f1afe5dc6b27aa4c03",
+    "url": "/todoapp/static/js/runtime-main.9f4f9856.js"
+  }
+]);
